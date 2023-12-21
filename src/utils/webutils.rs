04520224a132cc2a4a27b66e2be69d758c7e00ed
viggo-gascou <94069687+viggo-gascou@users.noreply.github.com>
2023-12-21 18:53:54 +0100

    ✨ feat(webutils.rs): add more webutils - more getters and initial httpclient

--- src/utils/webutils.rs
+++ src/utils/webutils.rs
@@ -1,6 +1,10 @@
 use crate::utils::config::Config;
 
+use color_eyre::{eyre, eyre::Context, Report};
 use regex::Regex;
+use reqwest::Client;
+
+const USER_AGENT: &str = concat!(env!("CARGO_PKG_REPOSITORY"), "/", env!("CARGO_PKG_VERSION"));
 
 pub fn is_problem_id(problem: &str) -> bool {
     /* Matches any alphanumeric character or period - so that it can match
@@ -9,6 +13,33 @@ pub fn is_problem_id(problem: &str) -> bool {
 }
 
 pub fn get_problem_url(problem: &str) -> String {
-    let host_name = Config::new().unwrap().kattisrc.kattis.hostname;
+    let host_name = Config::load().unwrap().kattisrc.kattis.hostname;
     format!("https://{}/problems/{}", host_name, problem)
+}
+
+pub fn get_submission_url(submission_id: &str) -> String {
+    let host_name = Config::load().unwrap().kattisrc.kattis.hostname;
+    format!("https://{}/submissions/{}", host_name, submission_id)
+}
+
+pub fn get_sample_url(problem: &str) -> String {
+    let host_name = Config::load().unwrap().kattisrc.kattis.hostname;
+    format!("https://{}/problems/{}/file/statement/samples.zip", host_name, problem)
+}
+
+#[derive(Debug)]
+pub struct HttpClient {
+    pub client: Client,
+}
+
+impl HttpClient {
+    pub fn new() -> Result<Self, Report> {
+        
+        let client = Client::builder()
+            .user_agent(USER_AGENT)
+            .build()
+            .wrap_err("🙀 Failed to create http client")?;
+        
+        Ok(HttpClient { client })
+    }
 }
