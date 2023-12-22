7722aae6a8342616946d8591720ab6fe911e2ade
viggo-gascou <94069687+viggo-gascou@users.noreply.github.com>
2023-12-22 17:02:30 +0100

    ✨ feat(utils): add more file and webutils

---
+++ src/utils/fileutils.rs
@@ -0,0 +1,97 @@
+use std::path::{Path, PathBuf};
+
+use color_eyre::{eyre, eyre::{Context, ContextCompat}, Report};
+
+use crate::{cli::Get, App};
+
+pub fn get_problem_dir(app: &App, problem: &str) -> Result<PathBuf, Report> {
+    let current_dir = std::env::current_dir().wrap_err("🙀 Failed to get current directory")?;
+    let problem_dir = current_dir.join(problem);
+
+    Ok(problem_dir)
+}
+
+pub fn get_test_dir(app: &App, problem: &str) -> Result<PathBuf, Report> {
+    let problem_dir = get_problem_dir(app, problem)?;
+    let test_dir = problem_dir.join("tests");
+
+    if !test_dir.exists() {
+        return Err(eyre::eyre!("👀 Looks like you already have tests for {problem}"));
+    } else {
+        Ok(test_dir)
+    }
+}
+
+pub fn copy_template(app: &App, args: &Get, problem: &str) -> Result<(), Report> {
+    let problem_dir = get_problem_dir(app, problem)?;
+    let template_dir = PathBuf::from(format!(
+        "{}/templates",
+        app.config.internal_config.config_location
+    ));
+    let config = &app.config.kat_config;
+
+    // find the language for the problem by first checking the command line args
+    // if none is found, use the default language from the config
+    // if command line arg is found, check if it is a valid language i.e., there is an entry for it in the config
+    // the
+    // if it is not a valid language, throw an error
+    // if it is a valid language, use it
+    let language = match &args.language {
+        Some(lang) => {
+            if config.languages.contains_key(lang) {
+                lang
+            } else {
+                eyre::bail!("🙀 Invalid language: {}", lang);
+            }
+        }
+        None => &config.default.language,
+    };
+
+    let template_path = match config.languages.get(language) {
+        Some(lang) => {
+            if let Some(template_file) = &lang.template {
+                // join the template file with the template directory to get the full path to the template file
+                Some(template_dir.join(template_file))
+            } else {
+                println!("👀 No template file found for language: {}", language);
+                None
+            }
+        }
+        None => {
+            println!("👀 No template file found for language: {}", language);
+            None
+        }
+    };
+
+    // if there is a template file, copy it to the problem directory and rename it to the problem id
+
+    // check that the template file exists
+    // if it does not exist, throw an error
+    // if it does exist, copy it to the problem directory and rename it to the problem id
+    
+    if let Some(template_path) = template_path {
+        if !Path::new(&template_path).exists() {
+            eyre::bail!("🙀 Template file does not exist: {}", template_path.display());
+        }
+        let template_file_name = template_path
+            .file_name()
+            .wrap_err("🙀 Failed to get file name from path")?;
+        let template_file_no_ext = template_path
+            .file_stem()
+            .wrap_err("🙀 Failed to get file name from path")?
+            .to_str()
+            .wrap_err("🙀 Failed to convert file name to string")?
+            .to_string();
+        let template_file_name = template_file_name
+            .to_str()
+            .wrap_err("🙀 Failed to convert file name to string")?
+            .to_string();
+        let problem_file_name = template_file_name.replace(&template_file_no_ext, problem);
+        let problem_file_path = problem_dir.join(problem_file_name);
+
+        std::fs::copy(template_path, problem_file_path)
+            .wrap_err("🙀 Failed to copy template file")?;
+    }
+
+    Ok(())
+}
--- src/utils/webutils.rs
+++ src/utils/webutils.rs
@@ -1,8 +1,8 @@
-use crate::utils::config::Config;
+use crate::{utils::Config, App};
 
 use color_eyre::{eyre, eyre::Context, Report};
 use regex::Regex;
-use reqwest::Client;
+use reqwest::{Client, StatusCode};
 
 const USER_AGENT: &str = concat!(env!("CARGO_PKG_REPOSITORY"), "/", env!("CARGO_PKG_VERSION"));
 
@@ -17,6 +17,10 @@ pub fn get_problem_url(problem: &str) -> String {
     format!("https://{}/problems/{}", host_name, problem)
 }
 
+pub fn get_problem_url_from_hostname(problem: &str, hostname: &str) -> String {
+    format!("https://{}/problems/{}", hostname, problem)
+}
+
 pub fn get_submission_url(submission_id: &str) -> String {
     let host_name = Config::load().unwrap().kattisrc.kattis.hostname;
     format!("https://{}/submissions/{}", host_name, submission_id)
@@ -27,6 +31,52 @@ pub fn get_sample_url(problem: &str) -> String {
     format!("https://{}/problems/{}/file/statement/samples.zip", host_name, problem)
 }
 
+pub async fn problem_exists(app: &App, problem: &str, hostname: &str) -> Result<bool, Report> {
+    let problem_url = get_problem_url_from_hostname(problem, hostname);
+    let response = app.http_client.client.get(&problem_url).send().await?;
+
+    match response.status() {
+        StatusCode::OK => Ok(true),
+        StatusCode::NOT_FOUND => Ok(false),
+        _ => {
+            let status = response.status();
+            eyre::bail!("🙀 Failed to get problem: {} - {}", problem, status)
+        }
+    }
+}
+
+pub fn check_change_hostname(app: &App, problem: &str) -> Result<String, Report> {
+    // if the problem id contains a period, it is probably a custom problem
+    // and we should change the hostname to the custom hostname
+    // eg. The problem itu.seatallocation is hosted on itu.kattis.com instead of open.kattis.com
+    // so we should change the hostname to itu.kattis.com before fetching the tests
+
+    let hostname = &app.config.kattisrc.kattis.hostname;
+    if problem.contains(".") {
+        let problem_hostname = problem.split(".").nth(0).unwrap();
+
+        println!(
+            "\n👀 It looks like problem {} is hosted on {}.kattis.com instead of {}.",
+            problem, problem_hostname, hostname
+        );
+        let change_hostname = dialoguer::Confirm::new()
+            .with_prompt(format!(
+                "Do you want to change the hostname to {}.kattis.com?",
+                problem_hostname
+            ))
+            .interact()
+            .unwrap();
+
+        if change_hostname {
+            Ok(format!("{}.kattis.com", problem_hostname))
+        } else {
+            Ok(hostname.to_string())
+        }
+    } else {
+        Ok(hostname.to_string())
+    }
+}
+
 #[derive(Debug)]
 pub struct HttpClient {
     pub client: Client,
@@ -34,12 +84,11 @@ pub struct HttpClient {
 
 impl HttpClient {
     pub fn new() -> Result<Self, Report> {
-        
         let client = Client::builder()
             .user_agent(USER_AGENT)
             .build()
             .wrap_err("🙀 Failed to create http client")?;
-        
+
         Ok(HttpClient { client })
     }
 }
