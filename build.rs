c24a3da3fcebecfbb34977802d7bc608ab486be5
viggo-gascou <94069687+viggo-gascou@users.noreply.github.com>
2024-01-10 16:12:30 +0100

    ✨ feat(build.rs): add build script for generating completions at compile time

---
+++ build.rs
@@ -0,0 +1,23 @@
+use clap::CommandFactory;
+use clap_complete::{generate_to, Shell::Bash, Shell::Elvish, Shell::Fish, Shell::Zsh};
+use std::{env, error::Error};
+
+include!("src/cli.rs");
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let outdir = std::path::Path::new(env!("CARGO_MANIFEST_DIR")).join("completions");
+    if !outdir.exists() {
+        std::fs::create_dir_all(&outdir)?;
+    }
+
+    let mut cmd = Cli::command();
+
+    let sup_shells = [Bash, Zsh, Fish, Elvish];
+
+    for &shell in sup_shells.iter() {
+        generate_to(shell, &mut cmd, "kat", &outdir)?;
+        println!("Generated completion file for {} shell", shell);
+    }
+
+    Ok(())
+}
