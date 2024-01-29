fc78cc1b6f4dd402783208714406ee9054b2dfcd
Viggo Gascou <94069687+viggo-gascou@users.noreply.github.com>
2024-01-29 01:03:14 +0100

    🐛 fix(scripts): forgot macos grep is limited

--- scripts/download-latest.sh
+++ scripts/download-latest.sh
@@ -4,10 +4,11 @@
 latest_release=$(curl --silent "https://api.github.com/repos/viggo-gascou/kat-rs/releases/latest")
 
 # Get the tag name of the latest release assuming semvar format (vX.Y.Z)
-tag_name=$(echo $latest_release | grep -Po '"tag_name": "\Kv[0-9]+\.[0-9]+\.[0-9]+(?=")')
+tag_name=$(echo $latest_release | sed -n 's/.*"tag_name": "\(v[0-9]*\.[0-9]*\.[0-9]*\)",.*/\1/p')
 
 # Construct the filename
 filename="kat-${tag_name}-$(uname)-$(uname -m)"
+echo https://github.com/viggo-gascou/kat-rs/releases/download/${tag_name}/${filename}
 
 # Download the file
-curl -L -O "https://github.com/viggo-gascou/releases/latest/download/${filename}" -o "kat"
+curl -L "https://github.com/viggo-gascou/kat-rs/releases/download/${tag_name}/${filename}" -o kat-$(uname)-$(uname -m)
