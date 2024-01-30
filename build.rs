23c658db4c191dc35b8f591b6554a8d0f2dc2d1f
Viggo Gascou <94069687+viggo-gascou@users.noreply.github.com>
2024-01-30 19:21:59 +0100

    ♻️ refactor(completions): remove completions - upload as part of releases instead

--- build.rs
+++ build.rs
@@ -1,20 +1,18 @@
 use clap::CommandFactory;
-use clap_complete::{
-    generate_to, Shell::Bash, Shell::Elvish, Shell::Fish, Shell::Zsh,
-};
+use clap_complete::{generate_to, Shell::Bash, Shell::Fish, Shell::Zsh};
 use std::{env, error::Error};
 
 include!("src/cli.rs");
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let outdir = std::path::Path::new(env!("CARGO_MANIFEST_DIR")).join("completions");
-    if !outdir.exists() {
-        std::fs::create_dir_all(&outdir)?;
-    }
+    let outdir = match env::var_os("OUT_DIR") {
+        None => return Ok(()),
+        Some(outdir) => outdir,
+    };
 
     let mut cmd = Cli::command();
 
-    let shells = [Bash, Zsh, Fish, Elvish];
+    let shells = [Bash, Zsh, Fish];
 
     for &shell in shells.iter() {
         generate_to(shell, &mut cmd, "kat", &outdir)?;
